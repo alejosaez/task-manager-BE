9ee96202e176e125f81ea0c2480bd0acfa9053ce
Alejo Saez Gebicki <alejosaezgebicki@gmail.com>
2024-12-28 17:27:55 -0300

    creating swagger for api documentation

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,10 +1,26 @@
-import { IsString, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsOptional, IsBoolean } from 'class-validator';
 
 export class CreateTaskDto {
+  @ApiProperty({ description: 'Título de la tarea', example: 'Comprar pan' })
   @IsString()
   title: string;
 
+  @ApiProperty({
+    description: 'Descripción de la tarea',
+    example: 'Ir a la panadería a las 9 AM',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   description?: string;
+
+  @ApiProperty({
+    description: 'Estado de la tarea (completada o no)',
+    example: false,
+    required: false,
+  })
+  @IsOptional()
+  @IsBoolean()
+  completed?: boolean;
 }
