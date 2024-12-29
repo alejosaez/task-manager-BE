c09c50facbf2e4145fa6727da50adf688729b27c
Alejo Saez Gebicki <alejosaezgebicki@gmail.com>
2024-12-29 00:01:36 -0300

    add to .env for mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,13 @@
-// import { Module } from '@nestjs/common';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { TasksModule } from './tasks/tasks.module';
-// import { ConfigModule } from '@nestjs/config';
-
-// console.log('Valor de MONGO_URI:', process.env.MONGO_URI);
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot({ isGlobal: true }),
-//     MongooseModule.forRoot(process.env.MONGO_URI),
-//     TasksModule,
-//   ],
-// })
-// export class AppModule {}
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
 import { ConfigModule } from '@nestjs/config';
 
-// Cambia temporalmente a la conexión directa para pruebas
-const mongoUri =
-  'mongodb+srv://alejosaezgebicki:XEVv1dn9XFhb27lt@cluster0.uf3ya.mongodb.net/taskManager?retryWrites=true&w=majority';
-
-console.log('Valor de MONGO_URI:', mongoUri);
+console.log('Valor de MONGO_URI:', process.env.MONGO_URI);
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(mongoUri), // Aquí pasamos directamente la URI
+    MongooseModule.forRoot(process.env.MONGO_URI),
     TasksModule,
   ],
 })
