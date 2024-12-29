ec7c628d3233592595c658575380723d10c0b519
Alejo Saez Gebicki <alejosaezgebicki@gmail.com>
2024-12-28 23:25:02 -0300

    Chore: Added diagnostics for production environment and temporary route for testing connectivity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
 import { ConfigModule } from '@nestjs/config';
-
 
 console.log('Valor de MONGO_URI:', process.env.MONGO_URI);
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    // MongooseModule.forRoot(process.env.MONGO_URI),
     TasksModule,
   ],
 })
